d972b24a2c746cdfbf5245f1e81fbb76b05ec25c
Adam Bukowski <bukowski.adam@gmail.com>
2020-03-23 23:25:02 +0100

    feat: add images, remove console.log

--- src/useAnimationFrame.js
+++ src/useAnimationFrame.js
@@ -3,8 +3,6 @@ import { useEffect, useRef } from 'react';
 export const useAnimationFrame = (callback) => {
   const requestRef = useRef();
 
-  console.log('animation frame');
-
   const animate = () => {
     callback();
 
